03aa272a00103ea5889e7f908ee319110f3be559
ArditioReihansyah <arditio.reihansyah11@ui.ac.id>
2022-10-27 08:50:53 +0700

    adding get_dokter_name

--- Authentication/models.py
+++ Authentication/models.py
@@ -19,6 +19,10 @@ class Dokter(models.Model):
     phone_number = models.CharField(max_length=20)
     workPlace = models.CharField(max_length=100)
     workingDays = models.CharField(max_length=50)
+    
+    def get_dokter_name(self):
+        return self.user.first_name + " " + self.user.last_name
+
 
 class Rumahsakit(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE, primary_key = True)
