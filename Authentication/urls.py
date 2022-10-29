5fd2757e7ae8bbe0b6e6fa45364ccbd1ab546dc6
ArditioReihansyah <arditio.reihansyah11@ui.ac.id>
2022-10-29 20:17:18 +0700

    role done

--- Authentication/urls.py
+++ Authentication/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('registerdokter/', views.registerDokter, name="registerdokter"),
     path('login/', views.login_user, name="login"),
     path('logout/', views.logout_user, name="logout"),
+    path("showjson", views.show_json, name="show_json"),
 ]
--- Authentication/views.py
+++ Authentication/views.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 import re
 from django.contrib import messages
+from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreatePasienForm, CreateDokterForm
@@ -29,6 +31,7 @@ def registerDokter(request):
     if request.method == 'POST':
         form = CreateDokterForm(request.POST)
         if form.is_valid():
+            form.instance.role = 2
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, 'Account was created for ' + username)
@@ -40,15 +43,32 @@ def login_user(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('HIVCenter:show_homepage')
+        if request.user.is_authenticated:
+            messages.info(request, 'Anda sedang login!')
         else:
-            messages.info(request, 'Username atau Password Salah!')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                response =  redirect('HIVCenter:show_homepage')
+                response.set_cookie("last_login", str(datetime.now()))
+                return response
+            else:
+                messages.info(request, 'Username atau Password Salah!')
     context = {}
     return render(request, 'login.html', context)
 
 def logout_user(request):
     logout(request)
     return redirect('Authentication:chooseregisteras')
+
+def show_json(request):
+    return JsonResponse(
+        {
+            "is_login": request.user.is_authenticated,
+            "role": request.user.role if request.user.is_authenticated else None,
+            "username": request.user.username
+            if request.user.is_authenticated
+            else None,
+        },
+        status=200,
+    )
