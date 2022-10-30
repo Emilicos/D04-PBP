49464c1325d704cb907c0223f82ac2033ddf7bc0
carlenee <carleneannabel@gmail.com>
2022-10-30 17:54:20 +0700

    add ajax POST and fix styling

--- Experience/forms.py
+++ Experience/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from bootstrap_modal_forms.forms import BSModalModelForm
 
 from .models import *
 
--- Experience/views.py
+++ Experience/views.py
@@ -6,18 +6,21 @@ from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import get_object_or_404, redirect
 from django.core import serializers
+import datetime  
 
 # Create your views here.
+
+
 def show_experience(request):
     posts = Experience.objects.all()
     if request.user.is_authenticated:
         print(request.user.role)
         if request.user.role == 1:
-            return render(request, 'experience.html', {'posts': posts})
+            return render(request, 'experience.html', {'posts': posts,'form':ExperienceForm()})
         else:
             return render(request, 'experience-umum.html', {'posts': posts})
     else:
-        return render(request, 'experience-umum.html', {'posts': posts})
+        return render(request, 'experience-umum.html', {'posts': posts,})
         
     
 
@@ -29,20 +32,30 @@ def show_experience_detail(request, id):
         return render(request, 'experience-detail-umum.html', {'posts': post})
        
 
-def create_experience(request):
-    form = ExperienceForm()
-    new_task = None
+# def create_experience(request):
+#     form = ExperienceForm()
+#     new_task = None
+#     if request.method == 'POST':
+#         form = ExperienceForm(request.POST)
+#         if form.is_valid():
+#             new_task = form.save(commit=False)
+#             new_task.user = request.user
+#             new_task.username=request.user.username
+#             new_task.save()
+#         return HttpResponseRedirect(reverse("Experience:show_experience"))
+#         # return HttpResponseRedirect(reverse("todolist:show_todolist"))
+#     context = {'form': form}
+#     return render(request, 'experience-form.html', context)
+
+def create_experience_ajax(request):
     if request.method == 'POST':
-        form = ExperienceForm(request.POST)
-        if form.is_valid():
-            new_task = form.save(commit=False)
-            new_task.user = request.user
-            new_task.username=request.user.username
-            new_task.save()
-        return HttpResponseRedirect(reverse("Experience:show_experience"))
-        # return HttpResponseRedirect(reverse("todolist:show_todolist"))
-    context = {'form': form}
-    return render(request, 'experience-form.html', context)
+        title = request.POST.get('title')
+        experience = request.POST.get('experience')
+        user = request.user
+        date= datetime.datetime.now()
+        item = Experience(user = user, username=user.username, posted=date, title=title,preview="", experience=experience)
+        item.save()
+        return JsonResponse({"Message": "Task Success"},status=200)
 
 def show_experience_json(request):
     experience = Experience.objects.all()
