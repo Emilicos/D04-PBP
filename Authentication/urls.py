ef34c9af393aa6a03812a9cb5ba48320585ec362
ArditioReihansyah <arditio.reihansyah11@ui.ac.id>
2022-11-02 04:12:56 +0700

    ajax register login template sementara

--- Authentication/urls.py
+++ Authentication/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('registerpasien/', views.registerPasien, name="registerpasien"),
     path('registerdokter/', views.registerDokter, name="registerdokter"),
     path('login/', views.login_user, name="login"),
+    path('login/validate_login/', views.validate_login, name="validateLogin"),
     path('logout/', views.logout_user, name="logout"),
     path("showjson/", views.show_json, name="show_json"),
+    path('registerpasien/validate_username/', views.validate_username, name='validate_username'),
 ]
