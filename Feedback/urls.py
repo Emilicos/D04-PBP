7d7965d42bea64e230340f24d567d6a0b7b8d216
ravena.meilani <ravena.meilani@ui.ac.id>
2022-11-02 21:36:56 +0700

    feedbackkkkkk

--- Feedback/urls.py
+++ Feedback/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-
-from Feedback.views import create_feedback, show_feedback, show_feedback_json
+from Feedback.views import create_feedback
+from Feedback.views import show_feedback
+from Feedback.views import show_feedback_json
+from Feedback.views import delete
 
 app_name = "feedback"
 urlpatterns = [
-    path("", show_feedback, name = "show_feedback"),
-    path("create/", create_feedback, name = "create_feedback"),
-    path("json/", show_feedback_json, name = "show_feedback_json"),
+    path('', show_feedback, name = 'show_feedback'),
+    path('create/', create_feedback, name = 'create_feedback'),
+    path('json/', show_feedback_json, name = 'show_feedback_json'),
+    path('delete/<int:pk>/', delete, name='delete'),
 ]
