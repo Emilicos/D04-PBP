ae32a4ed74aad987d7721807b40b8d2ae8598a82
carlenee <carleneannabel@gmail.com>
2022-11-30 00:00:55 +0700

    adjust for flutter experience (coba)

--- Experience/urls.py
+++ Experience/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from Experience.views import create_experience_ajax, show_experience, show_experience_detail, show_experience_json
+from Experience.views import create_experience_ajax, show_experience, show_experience_detail, show_experience_json,add_experience_flutter
 
 app_name = "Experience"
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('create-experience/', create_experience_ajax, name='create_experience_ajax'),
     path('experience-detail/<int:id>/', show_experience_detail, name='show_experience_detail'),
     path('json/', show_experience_json, name='show_experience_json'),
+    path('add-experience-flutter', add_experience_flutter, name='add_experience_flutter')
 ]
