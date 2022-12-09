2ddaff413763ad19ffd3956f6457f19ffdde3f80
emilicos <notalvaroaustin@gmail.com>
2022-12-09 14:11:45 +0700

    didnt work

--- Blogpost/views.py
+++ Blogpost/views.py
@@ -170,16 +170,8 @@ def update_blogpost(request, id):
 def delete_blogpost(request, id):
     print("masuk delete")
     if(request.method == "DELETE"):
-        try:
-            blogpost = BlogpostModel.objects.get(pk = id)
-            if(blogpost.user == request.user.id):
-                blogpost.delete()
-                return JsonResponse({
-                    "message": "Delete berhasil"
-                })
-            else:
-                return JsonResponse({
-                    "message": "Anda bukan user yang membuat!"
-                })
-        except BlogpostModel.DoesNotExist:
-            return Http404
+        blogpost = BlogpostModel.objects.get(pk = id)
+        blogpost.delete()
+        return JsonResponse({
+            "message": "Delete berhasil"
+        })
