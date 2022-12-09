b774a7ee7f1824f785569d2bd68c0366a8a6ee79
nataniadeandra <nataniadeandra@gmail.com>
2022-12-09 22:50:46 +0700

    tes

--- Booking/urls.py
+++ Booking/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from Booking.views import add_booking, delete_booking, get_dokter_json, show_booking, show_json
+from Booking.views import add_booking, add_booking_flutter, delete_booking, get_dokter_json, list_nama_dokter, show_booking, show_json, show_json_flutter
 
 app_name = 'Booking'
 
@@ -9,4 +9,6 @@ urlpatterns = [
     path('add/', add_booking, name='add_booking'),
     path('delete/<int:id>/', delete_booking, name='delete_booking'),
     path('all/', get_dokter_json, name="get_dokter_json"),
+    path('nama_dokter/', list_nama_dokter, name="list_nama_dokter"),
+    
 ]
